a4e2fd0ce3dd15df004f93c07cd1b74b08b613af
S.Rinat <102806560+07Rinat07@users.noreply.github.com>
2026-08-05 20:45:15 +0500

    feat(player): depend on isolated LibVLC fallback

--- feature/player/build.gradle.kts
+++ feature/player/build.gradle.kts
@@ -37,6 +37,7 @@ dependencies {
     implementation(project(":core:domain"))
     implementation(project(":core:model"))
     implementation(project(":core:player"))
+    implementation(project(":core:player-vlc"))
     implementation(project(":core:utils"))
 
     implementation(libs.androidx.core.ktx)
