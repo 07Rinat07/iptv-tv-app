d481dde9a097b4ae7e9ef2af446e356d4bf21006
S.Rinat <102806560+07Rinat07@users.noreply.github.com>
2026-08-21 18:01:52 +0500

    fix(playlists): declare activity compose dependency

--- feature/playlists/build.gradle.kts
+++ feature/playlists/build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.lifecycle.runtime.compose)
